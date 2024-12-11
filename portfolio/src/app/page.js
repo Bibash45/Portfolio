fa75f2c802c0eb3474693dbd91bddb74cc696555
Bibash45 <bibashcdry46@gmail.com>
2024-12-11 13:09:07 +0545

    call to action

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -4,6 +4,7 @@ import HomeComponent from "./components/HomeComponent/HomeComponent";
 import MySlider from "./components/SliderCard/MySlider";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Testimonial from "./components/Testimonial/Testimonial";
+import CallToAction from "./components/CallToAction/CallToAction";
 const hind = Hind({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
@@ -29,21 +30,29 @@ export default function Home() {
           >
             Portfolio
           </p>
-          <div   style={{ transform: "translate(0px,-250px" }}>
-            <p className="text-[#48AFDE] md:pl-[80px] px-5 font-extrabold text-5xl">Recent Works</p>
-            <p className={`max-w-2xl md:pl-[80px] px-5 text-[16px] text-[#47626D] leading-8 mt-5 ${hind.className}`}
+          <div style={{ transform: "translate(0px,-250px" }}>
+            <p className="text-[#48AFDE] md:pl-[80px] px-5 font-extrabold text-5xl">
+              Recent Works
+            </p>
+            <p
+              className={`max-w-2xl md:pl-[80px] px-5 text-[16px] text-[#47626D] leading-8 mt-5 ${hind.className}`}
             >
-              Here are a few of my recent work As a web designer and full-stack web developer , I constantly prioritise 100% client satisfaction . I always enjoy working on my project, i attempt to learn new things. As a result , my painting are always current and one-of-a-kind.
+              Here are a few of my recent work As a web designer and full-stack
+              web developer , I constantly prioritise 100% client satisfaction .
+              I always enjoy working on my project, i attempt to learn new
+              things. As a result , my painting are always current and
+              one-of-a-kind.
             </p>
           </div>
         </div>
-        <div style={{ transform: "translate(0px,-170px" }} >
+        <div style={{ transform: "translate(0px,-170px" }}>
           <MySlider />
         </div>
       </div>
 
       <AboutMe />
       {/* <Testimonial /> */}
+      <CallToAction />
     </main>
   );
 }
