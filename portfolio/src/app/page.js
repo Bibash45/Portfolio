e4cb177421f39bb05a8c341d8b7c9317c03f8c9c
Bibash45 <bibashcdry46@gmail.com>
2024-12-11 15:55:47 +0545

    error fixed

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
       <HomeComponent />
       <div
         id="portfolio"
-        className="mt-0 pt-5 -mb-40"
+        className="mt-[270px] md:mt-[350px] lg:mt-[350px] xl:mt-[350px] pt-5 xl:pt-20 "
         style={{
           backgroundImage:
             "linear-gradient(-62deg, #EE7FB 0 45%, white 0% 100%),",
@@ -24,12 +24,12 @@ export default function Home() {
         }}
       >
         <div className="container m-auto">
-          <p
+          {/* <p
             className="text-[300px] text-[#F7FBFD] md:pl-[50px] px-5 max-w-[750px] w-[100%]  "
             style={{ transform: "translate(0px,-20px" }}
           >
             Portfolio
-          </p>
+          </p> */}
           <div style={{ transform: "translate(0px,-250px" }}>
             <p className="text-[#48AFDE] md:pl-[80px] px-5 font-extrabold text-5xl">
               Recent Works
